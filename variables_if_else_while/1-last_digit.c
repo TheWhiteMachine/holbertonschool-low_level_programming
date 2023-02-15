40e3ec489acbdeea9ea02aaec40b31221d78899d
TheWhiteMachine <priveiro@gmail.com>
2023-02-15 12:55:06 -0800

    type in text

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else if (n < 6)
 	{
-		printf("Last digit of %i is %i and is less than 6 and not 9\n", n, last);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 	}
 	else
 	{
