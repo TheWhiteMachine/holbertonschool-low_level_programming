d402a2331919c526de91bc3a500ac982132eff9a
TheWhiteMachine <priveiro@gmail.com>
2023-02-16 12:09:59 -0800

    task 1

---
+++ functions_nested_loops/_printalphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_alphabet - writes the alphabet
+ * @c: The character to print in int from the a char
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet(void)
+{
+	int  c = 97;
+
+	for (; c < 123; c++)
+	{
+	putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
