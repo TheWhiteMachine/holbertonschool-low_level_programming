906d72440aadc67f26932b996fa488962b02014b
TheWhiteMachine <priveiro@gmail.com>
2023-02-16 04:32:30 -0800

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+i#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return : Print the Alphabet  
+ */
+int main(void)
+{
+	int  = c;
+	
+	for (c = 97; c < 121; c++)
+	{
+	putchar(c)
+	}
+}
