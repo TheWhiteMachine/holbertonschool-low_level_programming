f741194803e11aef833bb4bff536b3b0110ea1c8
TheWhiteMachine <priveiro@gmail.com>
2023-02-16 06:02:18 -0800

    auto push

---
+++ variables_if_else_while/6-print_number.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Print the Alphabet
+*/
+int main(void)
+{
+	int n = 48;
+
+	for (; n < 58; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
