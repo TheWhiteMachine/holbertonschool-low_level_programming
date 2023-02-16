7b8dae108ec29dc011de2241ea735bffe5d47c2a
TheWhiteMachine <priveiro@gmail.com>
2023-02-16 05:40:40 -0800

    auto push

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Print the Alphabet
+*/
+int main(void)
+{
+	for (int n = 0; n <10; n++)
+	{
+	printf ("%d",n);
+	}
+	retunr (0);
+}
