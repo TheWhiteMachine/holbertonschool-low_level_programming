45f424fc5a60745cc07f02dd924b47c3ef78d031
TheWhiteMachine <priveiro@gmail.com>
2023-02-16 06:07:17 -0800

    auto push

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Print the Alphabet
+*/
+int main(void)
+{
+	int  c = 121;
+
+	for (; c >= 97; c--)
+	{
+	putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
