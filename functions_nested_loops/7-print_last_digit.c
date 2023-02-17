560454b9a0f6bc7a16bbfac76675f2255c65a389
TheWhiteMachine <priveiro@gmail.com>
2023-02-17 08:15:11 -0800

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * Return: alwayes 0
+ * @n : an int
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	r = n % 10;
+	_putchar(r);
+}
