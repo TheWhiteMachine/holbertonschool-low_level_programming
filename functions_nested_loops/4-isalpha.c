3de7604beb5d0a18638bef33ed846ddee561aa75
TheWhiteMachine <priveiro@gmail.com>
2023-02-17 06:15:23 -0800

    alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isaplpha - check if a char is alphanumeric
+ * Return: On lowwer 1 in not lower 0
+ * On error, -1 is returned, and errno is set appropriately.
+ * @c : char
+ */
+
+int _isalpha(int c)
+{
+	int r;
+
+	if (c > 96 && c < 123)
+		r = 1;
+	else if (c > 64 && c < 91)
+		r = 1;
+	else if (c > 47 && c < 58)
+		r = 1;
+	else
+	       r = 0;
+	return (r);
+}
