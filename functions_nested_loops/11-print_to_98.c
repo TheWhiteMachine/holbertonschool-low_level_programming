b10eb2f0c4e16e4fad523d65be19f802911d946c
TheWhiteMachine <priveiro@gmail.com>
2023-02-18 19:34:57 -0800

    not save

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,25 +3,26 @@
 /**
  * print_to_98 - print from n to 98
  * On error, -1 is returned, and errno is set appropriately.
- * @n: an integer 
+ * @n: an integer
 */
 void print_to_98(int n)
 {
 	int i;
+
 	if (n == 98)
-		printf("%i%s", n, " ");
+		printf("%i", n);
 	if (n < 98)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%i", i);
-			if (i != 98)			
+			if (i != 98)	
 				printf("%s", ", ");
 		}
 	if (n > 98)
 		for (i = n; i >= 98; i--)
 		{
 			printf("%i", i);
-			if (i != 98)			
+			if (i != 98)	
 				printf("%s", ", ");
 		}
 	putchar('\n');
