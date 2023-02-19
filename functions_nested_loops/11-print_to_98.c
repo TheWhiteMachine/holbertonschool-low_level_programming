8603486f53a4ea9465a9461fc83e01e1438d1cb6
TheWhiteMachine <priveiro@gmail.com>
2023-02-18 19:20:51 -0800

    adition case wrong var

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,14 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%i", i);
-			if (n != 98)			
+			if (i != 98)			
 				printf("%s", ", ");
 		}
 	if (n > 98)
 		for (i = n; i >= 98; i--)
 		{
-			printf("%i", n);
-			if (n != 98)			
+			printf("%i", i);
+			if (i != 98)			
 				printf("%s", ", ");
 		}
 }
