1b3f391f99a630e0c791b40f8a44023de153b60e
TheWhiteMachine <priveiro@gmail.com>
2023-02-18 19:03:09 -0800

    use a counter equel to n

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 /**
  * print_to_98 - print from n to 98
- * Return: 0
  * On error, -1 is returned, and errno is set appropriately.
  * @n: an integer 
 */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n < 98)
-		for (n; n >= 98; n++)
+		for (i = n; i >= 98; i++)
 		{
-			printf("%i", n);
+			printf("%i", i);
 			printf("%s", ", ");
 		}
 	if (n > 98)
-		for (n; n <= 98; n--)
+		for (i = n; i <= 98; i--)
 		{
 			printf("%i", n);
 			printf("%s", ", ");
