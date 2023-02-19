fe8d54f8315a7cf1680c2154461116b569af9519
TheWhiteMachine <priveiro@gmail.com>
2023-02-18 17:09:33 -0800

    fixing operation

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,22 +10,22 @@ void times_table(void)
 	int x = 0;
 	int y = 0;
 	int t = 0;
+	int numbers[] = 1,2,3,4,5,6,7,8,9,;
 	int r = 0;
 
 
 	for (t = 0;t < 11; t++)
 	{
-		for (y = 0; y < 11; y++)
-		{	
 		for (x = 0; x < 11; x++)
-		{
-			r = x * y;
+		{	
+			r = x * numbers[y];
 			if (r > 9)
 				_putchar('0' + (r % 10));
 			else
 				_putchar('0' + r);
 			_putchar(',');
 		}
+		y += 1;
 		_putchar('\n');
 		}
 	}
