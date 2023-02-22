d1bdf259877242df35751d831b1d1c137736e6b5
TheWhiteMachine <priveiro@gmail.com>
2023-02-22 10:14:31 -0800

    auto push

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code.
+ *
+ * Return: 1 for c as a digit and 0 c to not.
+ * @a: an integer number
+ * @b: other integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
