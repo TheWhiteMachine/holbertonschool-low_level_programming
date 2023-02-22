109cb2e0559a046143953fb0df10c820bd36baa7
TheWhiteMachine <priveiro@gmail.com>
2023-02-22 10:57:10 -0800

    auto push

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code.
+ *
+ */
+
+void more_numbers(void)
+{
+	for (int o = 0; o <= 10; o++)
+	{
+		for (int i = 0; i <= 14; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
