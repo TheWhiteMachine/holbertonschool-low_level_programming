339c44f397035e4cea34bd5e5475eda47e390c10
TheWhiteMachine <priveiro@gmail.com>
2023-02-22 11:57:46 -0800

    auto push

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -17,12 +17,8 @@ void more_numbers(void)
 			if (i > 9)
 			{
 				_putchar('0' + (i / 10));
-				_putchar('0' + (i % 10));
 			}
-			else
-			{
 			_putchar ('0' +(i % 10));
-			}
 		}
 		_putchar('\n');
 	}
