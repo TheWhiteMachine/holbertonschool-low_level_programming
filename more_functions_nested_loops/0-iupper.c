40005e4407db47730f25c5a6682ddfee430302c5
TheWhiteMachine <priveiro@gmail.com>
2023-02-22 08:30:06 -0800

    auto push

--- more_functions_nested_loops/0-iupper.c
+++ more_functions_nested_loops/0-iupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper' - check the code.
+ * _isupper - check the code.
  *
  * Return: 1 for uppercase and 'c'c to not.
  * @c: an integer number
@@ -11,8 +11,5 @@ int _isupper(int c)
 	if (c >= 65 && c <= 90)
 		return (1);
 	else
-	{
-		c = c + '0';
 		return (0);
-	}
 }
