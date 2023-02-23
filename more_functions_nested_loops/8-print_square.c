be206035e0e1c6dc46ca1011859b070391c84b29
TheWhiteMachine <priveiro@gmail.com>
2023-02-23 08:29:30 -0800

    auto push

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,14 +2,14 @@
 
 /**
  * print_square - print a sqare with a size input
- * @size: integer 
+ * @size: integer
  */
 
 void print_square(int size)
 {
 	int x, y;
 
-	if (size !=0 && size > 0)
+	if (size != 0 && size > 0)
 	{
 		for (y = 0; y < size; y++)
 		{
