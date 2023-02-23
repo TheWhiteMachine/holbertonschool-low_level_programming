3cd964ef5f5e639fa978d28685ee64b086b9f7c8
TheWhiteMachine <priveiro@gmail.com>
2023-02-23 11:13:48 -0800

    auto push

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,22 +2,23 @@
 
 /**
  * print_triangle - check if a char is alphanumeric
- * @size: integer 
+ * @size: integer
  */
 
 void print_triangle(int size)
 {
 	int i, sp, ch, size_sp, size_ch;
+
 	size_sp = size - 1;
 	size_ch = size - size_sp;
 	sp = 0;
 	ch = 0;
 
-	if ( size != 0 && size > 0)
+	if (size != 0 && size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (sp = 0; sp <= size_sp; sp++)
+			for (sp = 1; sp <= size_sp; sp++)
 			{
 			putchar(32);
 			}
