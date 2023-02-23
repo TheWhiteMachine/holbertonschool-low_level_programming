a48c1b02846921d7ee9926675f0f205952fcfd92
TheWhiteMachine <priveiro@gmail.com>
2023-02-23 05:50:14 -0800

    auto push

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,15 +7,17 @@
 
 void print_diagonal(int n)
 {	
-	int i, o;
+	int i, o, steps;
 
 	if (n != 0 && n > 0)
 	{
-		for (i = 0; i <= n; i++)
+	steps = n;
+		for (i = 0; i < n; i++)
 		{
 		for (o = n; n - o; o--)
 			{
 			_putchar(32);
+			steps = n - o;
 			}
 		_putchar(92);
 		_putchar('\n');
