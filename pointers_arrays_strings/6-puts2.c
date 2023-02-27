7b12ed3d1495613943845b5aecc38e315573bb8d
TheWhiteMachine <priveiro@gmail.com>
2023-02-27 14:30:32 -0800

    auto push

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - print ever other element of a stirng
+ * @str:  a string
+ */
+void puts2(char *str)
+{
+	int i, n;
+	while (str[i])
+	{
+		i++;
+	}
+	for(n = 0; n <= i; n++)
+	{
+		if ((n % 2) == 0)
+		{
+			_putchar(str[n]);
+		}
+	}
+}
