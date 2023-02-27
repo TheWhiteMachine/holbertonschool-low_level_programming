8c2127520ab1ea9002f18213149d0b06aa20c76b
TheWhiteMachine <priveiro@gmail.com>
2023-02-27 14:47:17 -0800

    auto push

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - prints half of a string
+ * @str: a string
+ */
+void puts_half(char *str)
+{
+	int i, n, half;
+	i = 0;
+	n = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+	if ((i % 2) 0)
+		half = i / 2;
+	else
+		half = (i -1) / 2;
+	for ( n = half; n <= i; n++)
+	{
+	_putchar(str[n];
+	}
+	
+}
