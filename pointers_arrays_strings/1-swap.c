6ea7a5ebb3ce4deb9ca716e3fcd12a01a962e3e8
TheWhiteMachine <priveiro@gmail.com>
2023-02-27 06:15:42 -0800

    auto push

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the content of to variables using pinters
+ *
+ * @a: an integer.
+ * @b: an integer.
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*a = *c;
+	*a = *b;
+	*b = *c;
+}
