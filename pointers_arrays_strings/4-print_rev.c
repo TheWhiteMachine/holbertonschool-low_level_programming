5f2eb211c55c455503dafabca07d9f5cb286a849
TheWhiteMachine <priveiro@gmail.com>
2023-02-27 07:57:27 -0800

    auto push

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _print_rev - prints a string in reverse
+ * @s: a string
+ */
+void print_rev(char *s)
+{
+	int last = 0;
+
+	while (s[last])
+	{
+	last++;
+	}
+
+	while (last >= 0)
+	{
+		_putchar(s[last];
+		last--;
+	}
+	_putchar('\n');
+}
