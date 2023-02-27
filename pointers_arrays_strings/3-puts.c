fbed09baa243ac0dfcd58de660461f31defe0016
TheWhiteMachine <priveiro@gmail.com>
2023-02-27 07:35:07 -0800

    auto push

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string folloewed by a noew line
+ * @str: a string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+		_putchar(str[i];
+	}
+
+}
+
+
