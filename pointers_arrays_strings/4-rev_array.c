b7d3ed32738308c915349b6d8298edeabad99cd6
TheWhiteMachine <priveiro@gmail.com>
2023-02-28 13:05:19 -0800

    auto push

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,14 +9,19 @@ void reverse_array(int *a, int n)
 {
 	int b[1000];
 	int i = 0,
+	    x = 0,
 	    j = n - 1;
 
 	while (n > 0 && a[j])
 	{
-		b[i] = a[j];
+		for (; x == n / 2; x++)
+		{	
+		a[i] = b[i];
+		a[i] = a[j];
+		a[j] = b[i];
 		i += 1;
 		j -= 1;
+		}
 	}
-	b[n + 1] = '\0';
-	*a = *b;
+
 }
