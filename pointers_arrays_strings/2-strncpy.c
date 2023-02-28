8552d79b1de37842af866c9ee88c0c8ac8790820
TheWhiteMachine <priveiro@gmail.com>
2023-02-28 10:22:02 -0800

    auto push

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j = 0;	
-	
+	int j = 0;
+
 	if (src[j] != '\0')
 	{
-		for (j = 0; j <= n; j++)
+		for (j = 0; j < n; j++)
 		{
 		dest[j] = src[j];
 		}
