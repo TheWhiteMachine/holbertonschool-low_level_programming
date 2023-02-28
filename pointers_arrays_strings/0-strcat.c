30a5bbf0b05bdf91361c4dcc623c38cffb83ffb2
TheWhiteMachine <priveiro@gmail.com>
2023-02-28 07:07:04 -0800

    auto push

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *buff;
+	int i = 0,
+	    j = 0;
+
+	for (; dest[i] != '\0'; i++)
+	{
+		buff[i] = dest[i];
+	}
+	i += 1;
+	for (j = 0; src[j] '\0'; j++)
+	{
+		buff[i] = src[j];
+		i += 1;
+	}
+	src[j] = '\0';
+	*buff = *dest;
+	return (dest);
+}
