142777bb2f8ea58c9724fa0b6f8a6ff8751dcf4c
TheWhiteMachine <priveiro@gmail.com>
2023-02-28 04:35:44 -0800

    auto push

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print a string till a given n.
+ * @n: an integer.
+ * @a: a string.
+ */
+void print_array(int *a, int n)
+{
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d, ",a[i-]);
+	}
+
+}
