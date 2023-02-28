2edb4648718b2b4d60e0059e3b488053dbb30fbc
TheWhiteMachine <priveiro@gmail.com>
2023-02-28 11:54:12 -0800

    auto push

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_array - revert an array
+ * @a: a array of ints
+ * @n: int size of array
+ */
+void reverse_array(int *a, int n)
+{
+	int b[n];
+	int i = 1,
+	    j = n;
+
+	while (a[i]);
+	{
+		b[i] = a[j];
+		i += 1;
+		j -= 1;
+	}
+	*a = *b;
+}
