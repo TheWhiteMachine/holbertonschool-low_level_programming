2a34ce105dce2296418cede0a3a753479f8a319a
TheWhiteMachine <priveiro@gmail.com>
2023-02-27 17:03:12 -0800

    auto push

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,23 +5,28 @@
  * @str: a string
  */
 void puts_half(char *str)
+void puts2(char *str)
 {
-	int i, n, half;
+        int i, t, half;
+ 	i = 0;
+	t = 0;
 
-	i = 0;
-	n = 0;
-
-	while (str[i])
-	{
-		i++;
-	}
-	if ((i % 2) == 0)
-		half = i / 2;
-	else
-		half = (i - 1) / 2;
-	for (n = half; n <= i; n++)
-	{
-	_putchar(str[n]);
+        while (str[t])
+        {
+                t++;
+        }
+        t -= 1;
+        if ((t % 2) != 0)
+        {
+                half = (t-1) / 1;
+        }
+        else
+        {
+                half = half / 2;
+        }
+        for (; half < t; half++)
+        {
+                _putchar(str[half]);
 	}
 	_putchar('\n');
 }
