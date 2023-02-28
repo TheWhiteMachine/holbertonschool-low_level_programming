d5221219be57363107cd644f85564ee96b86ff41
TheWhiteMachine <priveiro@gmail.com>
2023-02-28 08:53:48 -0800

    auto push

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * _strcpy - concat two strings
+ *
+ * Return: return dest string
+ * @dest: destiny string
+ * @src: source string.
+ * @n: integer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0,
+	    j = 0;
+
+	while (i <= n)
+	{
+		dest[i] = src[j];
+		j += 1;
+		i += 1;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+
+
+}
