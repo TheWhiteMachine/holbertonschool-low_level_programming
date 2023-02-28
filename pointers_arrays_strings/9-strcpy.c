d1965bf0035289ae2b3cc92501cd61a892a7177e
TheWhiteMachine <priveiro@gmail.com>
2023-02-28 05:10:31 -0800

    auto push

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strccpy - copy string pinters
+ *
+ * Return: Always value of a string
+ * @dest: destiny string
+ * @src source string.
+ */
+
+char *_strcpy(char *dest, char *src);
+{
+	*dest = *src;
+}
+
