754e332b9b06c11082203773e63693f395425d00
TheWhiteMachine <priveiro@gmail.com>
2023-02-28 06:05:56 -0800

    auto push

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (; src[i] != '\n'; i++)
-	{			
+	for (; src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
 	}
-
+	dest{i} = '\0';
 
 	return (dest);
 }
