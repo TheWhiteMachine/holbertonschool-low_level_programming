e01ae3627847482d1933f109b019c0711daf4a95
TheWhiteMachine <priveiro@gmail.com>
2023-02-28 05:01:28 -0800

    auto push

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,7 +11,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == (n-1))
+		if (i == (n - 1))
 		{
 			printf("%d", a[i]);
 		}
@@ -20,6 +20,7 @@ void print_array(int *a, int n)
 			printf("%d, ", a[i]);
 		}
 	}
-	_putchar('\n');
+
+	printf('\n');
 
 }
