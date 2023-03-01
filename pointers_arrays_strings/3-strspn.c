0e1738462e8a5c59bd4cb0dcab53f3f8b4ae3a77
TheWhiteMachine <priveiro@gmail.com>
2023-03-01 10:27:57 -0800

    auto push

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * _strcmp - compare two strings.
+ *
+ * Return: positve or negative diference bt s2 and s1 or 0 on equals
+ * @s1: destiny string
+ * @s2: source string.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int b = 0;
+	int i,
+	    n;
+		
+	while (*s[n])
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				b++;
+				break;
+			}
+
+			else if (accept[i + 1] == '\0')
+				return (b);
+		}
+
+		n++;
+	}
+
+	return (b);
+}
