6504414519302031ecb8d3f9bb3cbfeb924278fc
TheWhiteMachine <priveiro@gmail.com>
2023-03-01 09:01:06 -0800

    auto push

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * memset - save a constant byte in memory pointed
+ * _memset - save a constant byte in memory pointed
  * @s: a string pointer
  * b: a char to fill with
  * @n: limit to fill the memory
