86d4e71b5f82a459ebe2891bcf7b6aebaadc3ea1
TheWhiteMachine <priveiro@gmail.com>
2023-03-01 10:11:02 -0800

    auto push

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,9 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 1;
+	int i = 1,
+	    n = 0;
 
-	for (i = 0; s[i]; i++)
+	while(s[n])
+	{
+		n++;
+	}
+	for (i = 0; i == n; i++)
 	{
 		if (s[i] == c)
 		{
