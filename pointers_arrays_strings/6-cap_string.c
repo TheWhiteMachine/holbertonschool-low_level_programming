d5181c5e8f7a555a6b965ccc315700ddbbc35d02
TheWhiteMachine <priveiro@gmail.com>
2023-03-01 05:50:46 -0800

    auto push

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- * _strncat - concat two strings till a gven n number.
+ * cap_string - capitalize words on a stringr.
  *
- * Return: return dest string
- * @dest: destiny string
- * @src: source string.
- * @n: integer
+ * Return: return poiinter of original string
+ * @s: a string
  */
 char *cap_string(char *s)
 {
@@ -13,14 +11,20 @@ char *cap_string(char *s)
 
 	while (s[i])
 	{
-		s[0] += 32;
-		if (s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '"' || s[i] == '(' || s[i] == ',' || s[i] == ')' || s[i] == ',' || s[i] == '{' || s[i] == 32 || s[i] == '.' || s[i] == '\n')
+		s[0] -= 32;
+		if (s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"')
 		{
 			if (s[i + 1] >= 97 && s[i + 1] <= 122)
 			{
 				s[i + 1] -= 32;
 			}
-
+i		}
+		if (s[i] == '"' || s[i] == '(' || s[i] == ',' || s[i] == ')' || s[i] == ',' || s[i] == '{' || s[i] == 32 || s[i] == '.' || s[i] == '\n')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] -= 32;
+			}
 		}
 	i++;
 	}
