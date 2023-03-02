d61357f3237b6060b25363ce41d349f41266d7a8
TheWhiteMachine <priveiro@gmail.com>
2023-03-02 10:29:54 -0800

    auto push

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -20,7 +20,7 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[i])
 		{
 			do {
-				if (needle[index + 1] == '\0')
+				if (needle[i + 1] == '\0')
 					return (haystack);
 
 				index++;
