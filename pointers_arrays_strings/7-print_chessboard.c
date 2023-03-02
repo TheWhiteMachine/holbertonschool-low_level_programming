4735ed9a1dbfc4eb929e35ff9a31834fe7bf8cd7
TheWhiteMachine <priveiro@gmail.com>
2023-03-02 11:19:47 -0800

    auto push

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -13,11 +13,10 @@ void print_chessboard(char (*a)[8])
 
 	for (; y < 8; y++)
 	{
-		for (; x < 8; x++)
+		for (x = 0; x < 8; x++)
 		{
-			printf("%c",a[x][y]);
+			printf("%c",a[y][x]);
 		}
-	printf("\n");
+		printf("\n");
 	}
-
 }
