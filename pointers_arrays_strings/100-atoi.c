11bb6745cefc051660abf6c905b3b4040f64fa68
TheWhiteMachine <priveiro@gmail.com>
2023-03-03 07:00:53 -0800

    auto push

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,34 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _atoi - check the code
+ * @s: a string of characters
+ * Return: an integer with numbers found.
  */
-int _atoi(char *s);
+int _atoi(char *s)
 {
+	int i = 0,
+	    j = 0,
+	    num = 0,
+	    signs = 0;
 
+
+	while (s[j])
+	{
+		if (s[j] >= '0' && s[j] <= '9')
+		{
+			num = (num * 10) + s[j] - '0';
+
+			if (s[j + 1]  < '0' || s[j +1] > '9' || s[j + 1] == '\0')
+				break;
+		}
+		if (s[j] == '-')
+			signs++;
+	j++;
+	}
+
+	if (signs % 2 == 0)
+		return (num);
+	else
+		return (-num);
 }
