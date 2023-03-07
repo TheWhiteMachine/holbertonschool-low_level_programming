574baf0efd8196da6f2ebeeead7e65a04bd179c8
TheWhiteMachine <priveiro@gmail.com>
2023-03-07 10:54:23 -0800

    auto push

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * mynameis - print its name
+ *
+ * Return: 1 on prime en 0 if not
+ * @n: the number in int
+ * @div: a divisor number to check
+ */
+int mynameis(int argc, char **argv)
+{
+	printf("%s",*argv[0]);
+	return(0);
+}
