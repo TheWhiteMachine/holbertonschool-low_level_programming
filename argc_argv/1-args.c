dd077d2e389720d162f93e2a659bce6ae0db9c45
TheWhiteMachine <priveiro@gmail.com>
2023-03-07 11:31:56 -0800

    auto push

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the number of arguments
+ * Return: Always 0
+ * @argc: counts arguments of program
+ * @argv: pointer to a array with the arguments
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%s\n", argc);
+	return (0);
+}
