cf14396d133c2a5db1c4236f59ac988f8f9c8fbc
TheWhiteMachine <priveiro@gmail.com>
2023-03-07 11:04:19 -0800

    auto push

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 /**
  * mynameis - print its name
  *
- * Return: 1 on prime en 0 if not
- * @n: the number in int
- * @div: a divisor number to check
+ * Return: 
+ * @argc cout or arguments
+ * @argv: pointer to a array of the arguments
  */
-int mynameis(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	printf("%s",*argv[0]);
 	return(0);
