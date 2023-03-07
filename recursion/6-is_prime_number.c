ad6ccd6efeabf4571df4ebc9c48fe795a7346e7e
TheWhiteMachine <priveiro@gmail.com>
2023-03-07 05:29:35 -0800

    auto push

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+/**
+ * test -  test if a number is prime
+ *
+ * Return: 1 on prime en 0 if not
+ * @n: the number in int
+ * @div: a divisor number to check
+ */
+
+int test(int n, int div)
+{
+
+	if (n < 2)
+	{	
+		return (0);
+	}
+	if (div == 1)
+	{
+		return (1);
+	}
+	if (n % div == 0)
+	{
+		return (0);
+	}
+	return (test(n, div - 1));
+}
+
+
+#include "main.h"
+
+
+/**
+ * is_prime_number - ask a function if a number is prime
+ *
+ * Return: 1 on prime en 0 if not
+ * @n: the number in int
+ */
+
+int is_prime_number(int n)
+{
+	return (test(n, n - 1));
+}
