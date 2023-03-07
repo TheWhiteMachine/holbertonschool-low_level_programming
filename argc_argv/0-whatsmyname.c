978e7f3870738090da528a7bf8d44511071a3578
TheWhiteMachine <priveiro@gmail.com>
2023-03-07 11:10:29 -0800

    auto push

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * mynameis - print its name
@@ -9,6 +10,7 @@
  */
 int main(int argc, char **argv)
 {
-	printf("%s",*argv[0]);
+	if (argc >= 1)
+		printf("%s",*argv[0]);
 	return(0);
 }
