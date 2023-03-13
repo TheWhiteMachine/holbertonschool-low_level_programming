3b3b2204240bdc0c936e4ba5f9cbd586d2a17a5c
TheWhiteMachine <priveiro@gmail.com>
2023-03-13 10:57:43 -0700

    auto push

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,13 +1,37 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _isdigit - check the code.
+ * _strdup - duplicate a string in alloc memory
  *
- * Return: 1 for c as a digit and 0 c to not.
- * @c: an integer number
+ * Return: 1 a pinter to dulplicated string
+ * @str: a string to duplicate
  */
 
 char *_strdup(char *str)
 {
+	unsigned int i = 0,
+		     len = 0;
+	char *string;
 
+	if (str == NULL)
+		return (0);
+
+	while (str[len] != '\0')
+		len++;
+
+	string = malloc(sizeof(char) * (len + 1));
+
+	if (string == NULL)
+		return (NULL);
+
+	while (i < len)
+	{
+		string[i] = str[i];
+		i++;
+	}
+
+	string[i] = '\0';
+
+	return (string);
 }
